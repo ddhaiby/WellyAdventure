b1ec75c4e5c8b13055f82fbbcc1633cf80e88b61
dany <dhad2301@usherbrooke.ca>
2023-12-03 21:34:51 -0500

    First pass of welly npc body size

--- src/scripts/Common/Characters/Npcs/WELLY_Npc.ts
+++ src/scripts/Common/Characters/Npcs/WELLY_Npc.ts
@@ -1,10 +1,18 @@
 import { WELLY_BaseScene } from "../../Scenes/WELLY_BaseScene";
 import { WELLY_Character } from "../WELLY_Character";
+import { WELLY_SpawnData } from "../WELLY_CharacterSpawner";
 
 export class WELLY_Npc extends WELLY_Character
 {
     constructor(scene: WELLY_BaseScene, x: number, y: number)
     {
         super(scene, x, y);
+    }
+
+    public init(spawnData?: WELLY_SpawnData): void
+    {
+        super.init(spawnData);
+
+        this.body.setSize(this.displayWidth, this.displayHeight);
     }
 }
